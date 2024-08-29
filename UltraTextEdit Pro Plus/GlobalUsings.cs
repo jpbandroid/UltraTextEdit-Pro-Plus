7664dae019753a3c485753cc8d627d4452eaaf25
ErrorTek <jakub.baranski.33@gmail.com>
2024-08-29 22:37:42 +0100

    Add project files.

---
+++ UltraTextEdit Pro Plus/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using Microsoft.UI;
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using UltraTextEdit_Pro_Plus.Common;
+global using UltraTextEdit_Pro_Plus.Views;
+global using WinUICommunity;
+global using static UltraTextEdit_Pro_Plus.Common.AppHelper;
---
+++ UltraTextEdit Pro Plus/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace UltraTextEdit_Pro_Plus.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+        appTitleBar.Window = App.CurrentWindow;
+    }
+}
+
