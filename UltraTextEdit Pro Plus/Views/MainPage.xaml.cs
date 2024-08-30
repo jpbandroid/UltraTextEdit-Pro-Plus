df2edf7577e8d92fa6c078e39dadbb67afc863d5
ErrorTek <jakub.baranski.33@gmail.com>
2024-08-30 08:18:46 +0100

    Add base UI

--- UltraTextEdit Pro Plus/Views/MainPage.xaml.cs
+++ UltraTextEdit Pro Plus/Views/MainPage.xaml.cs
@@ -7,5 +7,65 @@ public sealed partial class MainPage : Page
         this.InitializeComponent();
         appTitleBar.Window = App.CurrentWindow;
     }
+
+    private void Button_PointerEntered(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+
+    }
+
+    private void NewFile_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void OpenFile_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void OpenMultipleFiles_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Save_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Delete_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void HomeItem_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void EditTab_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void InsertTab_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ViewTab_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void SettingsButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void AboutItem_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
 }
 
