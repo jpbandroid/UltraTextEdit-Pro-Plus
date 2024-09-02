d7c08e4f71e465f28e7b2c374554472c1eb559d6
ErrorTek <jakub.baranski.33@gmail.com>
2024-09-02 16:14:58 +0100

    Add Ribbon

--- UltraTextEdit Pro Plus/Views/MainPage.xaml.cs
+++ UltraTextEdit Pro Plus/Views/MainPage.xaml.cs
@@ -67,5 +67,210 @@ public sealed partial class MainPage : Page
     {
 
     }
+
+    private void RTFPaste(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void SimplePaste(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Paste_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Cut_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Copy_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void FontBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void FontSize_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+    {
+
+    }
+
+    private void FontSize_GotFocus(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void FontSize_LostFocus(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void FontSizeBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void FontSizeBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ForegroundButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void NullForegroundButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Advanced_Color_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void HighlightButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void NullHighlightButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Advanced_Highlight_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Lowercase_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Uppercase_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Sentencecase_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Titlecase_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void EraseFormat_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template1_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template2_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template3_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template4_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template5_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template6_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template7_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template8_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template9_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template10_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template11_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Template12_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void BoldButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ItalicButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void UnderlineFlyoutItem_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void StrikethroughButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void SubscriptButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void SuperscriptButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void MenuFlyoutFontSize_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
 }
 
